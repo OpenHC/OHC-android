4e5812194c4ceb483081fd0404f7bfb93cd0f7eb
Tobias <tobleminer@gmail.com>
2015-02-05 01:36:27 +0100

    Added one more oo RPC

--- app/src/main/java/io/openhc/ohc/basestation/rpc/rpcs/Rpc_device_set_field_value.java
+++ app/src/main/java/io/openhc/ohc/basestation/rpc/rpcs/Rpc_device_set_field_value.java
@@ -2,30 +2,35 @@ package io.openhc.ohc.basestation.rpc.rpcs;
 
 import org.json.JSONObject;
 
+import java.util.Objects;
 import java.util.logging.Level;
 
 import io.openhc.ohc.basestation.Basestation;
 import io.openhc.ohc.basestation.rpc.Rpc_group;
 
-public class Rpc_device_get_field extends Rpc
+public class Rpc_device_set_field_value extends Rpc
 {
 	private String id;
 	private int field_id;
+	private Object value;
 
 	public final String RPC_METHOD = "device_get_field";
 	public final String RPC_ATTRIBUTE_ID = "device_id";
 	public final String RPC_ATTRIBUTE_FIELD_ID = "field_id";
+	public final String RPC_ATTRIBUTE_VALUE = "value";
 
-	public Rpc_device_get_field(Basestation bs, Rpc_group group)
+	public Rpc_device_set_field_value(Basestation bs, Rpc_group group)
 	{
-		this(bs, group, null, -1);
+		this(bs, group, null, -1, null);
 	}
 
-	public Rpc_device_get_field(Basestation bs, Rpc_group group, String id, int field_id)
+	public Rpc_device_set_field_value(Basestation bs, Rpc_group group, String id, int field_id,
+	                                  Object value)
 	{
 		super(bs, group);
 		this.id = id;
 		this.field_id = field_id;
+		this.value = value;
 	}
 
 	@Override
@@ -36,7 +41,8 @@ public class Rpc_device_get_field extends Rpc
 		{
 			json.put(this.RPC_ATTRIBUTE_METHOD, this.RPC_METHOD)
 					.put(RPC_ATTRIBUTE_ID, this.id)
-					.put(RPC_ATTRIBUTE_FIELD_ID, this.field_id);
+					.put(RPC_ATTRIBUTE_FIELD_ID, this.field_id)
+					.put(RPC_ATTRIBUTE_VALUE, this.value);
 		}
 		catch(Exception ex)
 		{
@@ -53,5 +59,10 @@ public class Rpc_device_get_field extends Rpc
 	public void set_field_id(int field_id)
 	{
 		this.field_id = field_id;
+	}
+
+	public void set_field_id(Object value)
+	{
+		this.value = value;
 	}
 }
